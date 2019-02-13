fe07445d3637e1d9dcd02198012027c0aae163be
Rob Larsen <rob@htmlcssjavascript.com>
2019-02-13 14:38:57 -0500

    rewriting this to be all fancy

--- assets/app.js
+++ assets/app.js
@@ -1,144 +1,127 @@
+"use strict";
+
 (function ($, undefined) {
+  var orgName = 'h5bp';
+  var stars = 0; // Return the repo url
 
-    var orgName = 'h5bp';
-    let stars = 0;
+  function getRepoUrl(repo) {
+    return repo.homepage || repo.html_url;
+  } // Return the repo description
 
-    // Return the repo url
-    function getRepoUrl(repo) {
-        return repo.homepage || repo.html_url;
-    }
 
-    // Return the repo description
-    function getRepoDesc(repo) {
-        return repo.description;
-    }
+  function getRepoDesc(repo) {
+    return repo.description;
+  } // Display a repo's overview (for recent updates section)
 
-    // Display a repo's overview (for recent updates section)
-    function showRepoOverview(repo) {
-        var item;
-        item = '<li>';
-        item +=     '<span class="name"><a href="' + repo.html_url + '">' + repo.name + '</a></span>';
-        item +=     ' &middot; <span class="time"><a href="' + repo.html_url + '/commits">' + html5prettyDate(repo.pushed_at) + '</a></span>';
-        item += '</li>';
 
-        $(item).appendTo("#updated-repos");
-    }
+  function showRepoOverview(repo) {
+    var item;
+    item = '<li>';
+    item += '<span class="name"><a href="' + repo.html_url + '">' + repo.name + '</a></span>';
+    item += ' &middot; <span class="time"><a href="' + repo.html_url + '/commits">' + html5prettyDate(repo.pushed_at) + '</a></span>';
+    item += '</li>';
+    $(item).appendTo("#updated-repos");
+  } // Create an entry for the repo in the grid of org repos
 
-    // Create an entry for the repo in the grid of org repos
-    function showRepo(repo) {
-        var $item = $('<div class="unit-1-3 repo" />');
-        var $link = $('<a class="box" href="' + getRepoUrl(repo) + '" />');
 
-        $link.append('<h2 class="repo__name">' + repo.name + '</h2>');
-        $link.append('<p class="repo__info">' + repo.watchers + ' stargazers ' + (repo.language !== null ? '&middot; ' + repo.language : '') + '</p>');
-        $link.append('<p class="repo__desc">' + getRepoDesc(repo) + '</p>');
+  function showRepo(repo) {
+    var $item = $('<div class="unit-1-3 repo" />');
+    var $link = $('<a class="box" href="' + getRepoUrl(repo) + '" />');
+    $link.append('<h2 class="repo__name">' + repo.name + '</h2>');
+    $link.append('<p class="repo__info">' + repo.watchers + ' stargazers ' + (repo.language !== null ? '&middot; ' + repo.language : '') + '</p>');
+    $link.append('<p class="repo__desc">' + getRepoDesc(repo) + '</p>');
+    $link.appendTo($item);
+    $item.appendTo('#repos');
+  }
 
-        $link.appendTo($item);
-        $item.appendTo('#repos');
-    }
+  $.getJSON('https://api.github.com/orgs/' + orgName + '/repos?callback=?', function (result) {
+    var repos = result.data;
+    $(function () {
+      $('#num-repos').text(repos.length); // Convert pushed_at to Date.
 
-    $.getJSON('https://api.github.com/orgs/' + orgName + '/repos?callback=?', function (result) {
-        var repos = result.data;
+      $.each(repos, function (i, repo) {
+        repo.pushed_at = new Date(repo.pushed_at);
+        var weekHalfLife = 1.146 * Math.pow(10, -9);
+        var pushDelta = new Date() - Date.parse(repo.pushed_at);
+        var createdDelta = new Date() - Date.parse(repo.created_at);
+        var weightForPush = 1;
+        var weightForWatchers = 1.314 * Math.pow(10, 7);
+        repo.hotness = weightForPush * Math.pow(Math.E, -1 * weekHalfLife * pushDelta);
+        repo.hotness += weightForWatchers * repo.watchers / createdDelta;
+      }); // Sort by hotness.
 
-        $(function () {
-            $('#num-repos').text(repos.length);
+      repos.sort(function (a, b) {
+        if (a.hotness < b.hotness) return 1;
+        if (b.hotness < a.hotness) return -1;
+        return 0;
+      });
+      $.each(repos, function (i, repo) {
+        stars += repo.stargazers_count;
 
-            // Convert pushed_at to Date.
-            $.each(repos, function (i, repo) {
-                repo.pushed_at = new Date(repo.pushed_at);
-
-                var weekHalfLife  = 1.146 * Math.pow(10, -9);
-
-                var pushDelta    = (new Date()) - Date.parse(repo.pushed_at);
-                var createdDelta = (new Date()) - Date.parse(repo.created_at);
-
-                var weightForPush = 1;
-                var weightForWatchers = 1.314 * Math.pow(10, 7);
-
-                repo.hotness = weightForPush * Math.pow(Math.E, -1 * weekHalfLife * pushDelta);
-                repo.hotness += weightForWatchers * repo.watchers / createdDelta;
-            });
-
-            // Sort by hotness.
-            repos.sort(function (a, b) {
-                if (a.hotness < b.hotness) return 1;
-                if (b.hotness < a.hotness) return -1;
-                return 0;
-            });
-
-            $.each(repos, function (i, repo) {
-              stars += repo.stargazers_count;
-
-              if (repo.archived === false){
-                console.log(false)
-                showRepo(repo);
-              }
-            });
-            $("#num-stargazers").text(stars.toLocaleString());
-            // Sort by most-recently pushed to.
-            repos.sort(function (a, b) {
-                if (a.pushed_at < b.pushed_at) return 1;
-                if (b.pushed_at < a.pushed_at) return -1;
-                return 0;
-            });
-
-            $.each(repos.slice(0, 3), function (i, repo) {
-                showRepoOverview(repo);
-            });
-        });
-    });
-
-    $.getJSON('https://api.github.com/orgs/' + orgName + '/members?per_page=100&callback=?', function (result) {
-        var members = result.data;
-        $(function () {
-            $('#num-members').text(members.length);
-        });
-    });
-
-    // Relative times
-    function prettyDate(rawdate) {
-        var date, seconds, formats, i = 0, f;
-        date = new Date(rawdate);
-        seconds = (new Date() - date) / 1000;
-        formats = [
-            [60, 'seconds', 1],
-            [120, '1 minute ago'],
-            [3600, 'minutes', 60],
-            [7200, '1 hour ago'],
-            [86400, 'hours', 3600],
-            [172800, 'Yesterday'],
-            [604800, 'days', 86400],
-            [1209600, '1 week ago'],
-            [2678400, 'weeks', 604800]
-        ];
-
-        while (f = formats[i ++]) {
-            if (seconds < f[0]) {
-                return f[2] ? Math.floor(seconds / f[2]) + ' ' + f[1] + ' ago' :  f[1];
-            }
+        if (repo.archived === false) {
+          console.log(false);
+          showRepo(repo);
         }
-        return 'A while ago';
+      });
+      $("#num-stargazers").text(stars.toLocaleString()); // Sort by most-recently pushed to.
+
+      repos.sort(function (a, b) {
+        if (a.pushed_at < b.pushed_at) return 1;
+        if (b.pushed_at < a.pushed_at) return -1;
+        return 0;
+      });
+      $.each(repos.slice(0, 3), function (i, repo) {
+        showRepoOverview(repo);
+      });
+    });
+  });
+  $.getJSON('https://api.github.com/orgs/' + orgName + '/members?per_page=100&callback=?', function (result) {
+    var members = result.data;
+    $(function () {
+      $('#num-members').text(members.length);
+    });
+  }); // Relative times
+
+  function prettyDate(rawdate) {
+    var date,
+        seconds,
+        formats,
+        i = 0,
+        f;
+    date = new Date(rawdate);
+    seconds = (new Date() - date) / 1000;
+    formats = [[60, 'seconds', 1], [120, '1 minute ago'], [3600, 'minutes', 60], [7200, '1 hour ago'], [86400, 'hours', 3600], [172800, 'Yesterday'], [604800, 'days', 86400], [1209600, '1 week ago'], [2678400, 'weeks', 604800]];
+
+    while (f = formats[i++]) {
+      if (seconds < f[0]) {
+        return f[2] ? Math.floor(seconds / f[2]) + ' ' + f[1] + ' ago' : f[1];
+      }
     }
 
-    // Wraps prettyDate in an HTML5 <time> element
-    function html5prettyDate(rawdate) {
-        return '<time datetime="' + rawdate.toISOString() + '">' + prettyDate(rawdate) + '</time>';
+    return 'A while ago';
+  } // Wraps prettyDate in an HTML5 <time> element
+
+
+  function html5prettyDate(rawdate) {
+    return '<time datetime="' + rawdate.toISOString() + '">' + prettyDate(rawdate) + '</time>';
+  }
+})(jQuery); // External 3rd party scripts
+
+
+(function (doc, script) {
+  var js,
+      fjs = doc.getElementsByTagName(script)[0],
+      add = function add(url, id) {
+    if (doc.getElementById(id)) {
+      return;
     }
 
-})(jQuery);
+    js = doc.createElement(script);
+    js.src = url;
+    id && (js.id = id);
+    fjs.parentNode.insertBefore(js, fjs);
+  }; // Twitter SDK
 
-// External 3rd party scripts
-(function(doc, script) {
-    var js,
-        fjs = doc.getElementsByTagName(script)[0],
-        add = function(url, id) {
-            if (doc.getElementById(id)) {return;}
-            js = doc.createElement(script);
-            js.src = url;
-            id && (js.id = id);
-            fjs.parentNode.insertBefore(js, fjs);
-        };
 
-    // Twitter SDK
-    add('https://platform.twitter.com/widgets.js', 'twitter-wjs');
-}(document, 'script'));
+  add('https://platform.twitter.com/widgets.js', 'twitter-wjs');
+})(document, 'script');
